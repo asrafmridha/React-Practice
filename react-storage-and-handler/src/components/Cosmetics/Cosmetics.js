c5a2e14b4f111a2d936f0be978611da84346c2fd
asraf-mridha <asrafmridha@gmail.com>
2024-02-18 23:09:55 +0600

    Import Default Single Import

---
+++ react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import add from '../../utilities/calculate';
+
+
+const Cosmetics = () => {
+    const first =10;
+    const second =20;
+    const total= add(first,second);
+    return (
+        <div>
+            <h1>Welcome To My Cosmetics Store</h1>
+            <p>Total Product {total}</p>
+        </div>
+    );
+};
+
+export default Cosmetics;
