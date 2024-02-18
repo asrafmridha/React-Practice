adb26da30042b4572cb7f88f667095e80b51b0f3
asraf-mridha <asrafmridha@gmail.com>
2024-02-18 23:18:02 +0600

    Import As A Object

--- react-storage-and-handler/src/components/Shoes/Shoes.js
+++ react-storage-and-handler/src/components/Shoes/Shoes.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { add, multiPly } from '../../utilities/calculate';
 
 const Shoes = () => {
+    const first=15;
+    const second=72;
+    const result =multiPly(first,second);
+    const sum =add(first,second);
     return (
         <div>
             <h3>This is Shoes Componenets </h3>
+            <p>Result : {result} and total : {sum}</p>
         </div>
     );
 };
--- react-storage-and-handler/src/utilities/calculate.js
+++ react-storage-and-handler/src/utilities/calculate.js
@@ -1,3 +1,6 @@
 const add =(first,second)=> first+second; 
+const multiPly =(first,second)=> first*second; 
 
-export default add;
+// export default add;
+
+export {add,multiPly}
