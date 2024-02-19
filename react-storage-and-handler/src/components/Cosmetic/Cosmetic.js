5559ce9851617f502277a9f34e7258eee7cdf81d
asraf-mridha <asrafmridha@gmail.com>
2024-02-19 22:45:09 +0600

    Create And Load Fake Data, Json Generator, Online Api

---
+++ react-storage-and-handler/src/components/Cosmetic/Cosmetic.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Cosmetic = (props) => {
+    const {name,price} =props.cosmetic;
+
+    return (
+        <div>
+            <h2>Buy This :{name}</h2>
+            <p>Only for ${price}</p>
+        </div>
+    );
+};
+
+export default Cosmetic;
--- react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
+++ react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { add } from '../../utilities/calculate';
+import Cosmetic from '../Cosmetic/Cosmetic';
 // import add from '../../utilities/calculate';
 
 
 const Cosmetics = () => {
-    const first =10;
-    const second =20;
-    const total= add(first,second);
+    // const first =10;
+    // const second =20;
+    // const total= add(first,second);
+    const [cosmetics,setCosmetics] =useState([]);
+    useEffect(()=>{
+        fetch("data.json")
+        .then(response=>response.json())
+        .then(data=>setCosmetics(data));
+
+    },[]);
     return (
         <div>
             <h1>Welcome To My Cosmetics Store</h1>
-            <p>Total Product {total}</p>
+            {
+                cosmetics.map(cosmetic=><Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>)
+            }
         </div>
     );
 };
