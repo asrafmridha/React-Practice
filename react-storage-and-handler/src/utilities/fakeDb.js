05ef13057194f2d958c4d9ca405c01edf7314ba5
asraf-mridha <asrafmridha@gmail.com>
2024-02-21 16:12:45 +0600

    Store Multiple Data In An Object With Local Storage

--- react-storage-and-handler/src/utilities/fakeDb.js
+++ react-storage-and-handler/src/utilities/fakeDb.js
@@ -1,11 +1,34 @@
 // use localStorage to save Data  
 const saveDataLocalStorage =(id)=>{
-    if(localStorage.getItem(id) !=null){
-        const newQuantity=parseInt(localStorage.getItem(id))+1;
-        localStorage.setItem(id,newQuantity);
+    // For Object Wise Save With Local Storage 
+    let shoppingCart={};
+
+    const storeCart =localStorage.getItem('shopping-cart');
+    if(storeCart){
+        shoppingCart=JSON.parse(storeCart);
     }else{
-        localStorage.setItem(id,1);
+        shoppingCart={};
     }
+
+    const quantity=shoppingCart[id];
+    if(quantity){
+        const newQuantity=quantity+1;
+        shoppingCart[id]=newQuantity;
+    }else{
+        shoppingCart[id]=1;
+    }
+
+    localStorage.setItem('shopping-cart',JSON.stringify(shoppingCart));
+
+    //For Single storage
+
+    // if(localStorage.getItem(id) !=null){
+    //     const newQuantity=parseInt(localStorage.getItem(id))+1;
+    //     localStorage.setItem(id,newQuantity);
+    // }else{
+  
+    //     localStorage.setItem(id,1);
+    // }
     
 }
 
