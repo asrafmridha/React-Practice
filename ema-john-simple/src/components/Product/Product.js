991cd11161a85cd9189f2fbcd999d3c0c36a56e5
asraf-mridha <asrafmridha@gmail.com>
2024-02-26 23:11:03 +0600

    Product Cart, Style Debug And Button Position

--- ema-john-simple/src/components/Product/Product.js
+++ ema-john-simple/src/components/Product/Product.js
@@ -8,7 +8,15 @@ const Product = (product) => {
     return (
         <div className='product'>
             <img src={img} alt="imgnai" />
-            <p>{name}</p>
+            <div className='product-info'>
+                <p>{name}</p>
+                <p>Price :{price}</p>
+                <p><small>Seller :{seller}</small></p>
+                <p><small>Ratings :{ratings}</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     );
 };
