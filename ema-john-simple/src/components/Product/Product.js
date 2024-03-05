073094c6babd9e3cc6544a230fe583907763d39d
asraf-mridha <asrafmridha@gmail.com>
2024-03-05 22:28:19 +0600

    Install External Packages, React-Fontawesome, Display Icon

--- ema-john-simple/src/components/Product/Product.js
+++ ema-john-simple/src/components/Product/Product.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import  './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
     const {name,img,seller,price,ratings} =props.product;
@@ -14,6 +16,7 @@ const Product = (props) => {
             </div>
             <button className='btn-cart' onClick={() =>props.handleAddToCart(props.product)}>
                 <p>Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
