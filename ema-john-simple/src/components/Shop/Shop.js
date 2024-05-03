2526b51cd5eb7cbc409a16f33ffbae9b7d605d5e
asraf-mridha <asrafmridha@gmail.com>
2024-05-03 22:17:03 +0600

    Setting The Cart Data On The LocalStorage For Future Retrieve

--- ema-john-simple/src/components/Shop/Shop.js
+++ ema-john-simple/src/components/Shop/Shop.js
@@ -2,28 +2,31 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-    useEffect(() =>{
+    useEffect(() => {
         fetch('products.json')
             .then(response => response.json())
             .then(data => setProducts(data))
-        }, []);
+    }, []);
 
-        const handleAddToCart =(product)=>{
-            const newCart=[...cart,product];
-            setCart(newCart);
 
-     
-        }
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+        addToDb(product.id);
+
+
+    }
     return (
         <div className='shop-container'>
             <div className="product-container">
                 {
-                    products.map(product=><Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
+                    products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
