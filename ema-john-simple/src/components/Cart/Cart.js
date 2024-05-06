6887b031550559497ecf5f1611562dcbfac1e880
asraf-mridha <asrafmridha@gmail.com>
2024-05-06 23:08:08 +0600

    Handle Quantity From Storage To Cart

--- ema-john-simple/src/components/Cart/Cart.js
+++ ema-john-simple/src/components/Cart/Cart.js
@@ -4,12 +4,15 @@ import './Cart.css'
 const Cart = (props) => {
     
     const {cart}=props;
+    console.log(cart);
     let total=0;
     let shipping=0;
+    let quantity=0;
 
     for(const product of cart ){
-        total+=product.price;
+        total+=product.price *product.quantity;
         shipping+=product.shipping;
+        quantity+=product.quantity;
     }
     const tax=(total*0.1).toFixed(2);
     const grandTotal= parseFloat((total+shipping+parseFloat(tax)));
@@ -17,7 +20,7 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h3>Order Summary </h3>
-            <p>Selected Item :{cart.length}</p>
+            <p>Selected Item :{quantity}</p>
             <p>Total Price :${total}</p>
             <p>Total Shipping :{shipping}</p>
             <p>Tax :{tax}</p>
