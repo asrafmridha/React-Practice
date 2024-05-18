5644801cb92422fe78d821a976e0a9609476b313
asraf-mridha <asrafmridha@gmail.com>
2024-05-18 23:15:38 +0600

    React Bootstarp install and ebeachbd Banner api intregreate

--- react-with-bootstarp/src/App.js
+++ react-with-bootstarp/src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import CardGroup from './components/CardGroup/CardGroup';
+import { Button } from 'react-bootstrap';
+import Carousel from './components/Carousel/Carousel';
+
+
 
 function App() {
   return (
     <div className="App">
-      <div class="spinner-border text-warning" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <CardGroup></CardGroup>
+      <Carousel></Carousel>
+      <button>Plain Button</button>
+      <Button>React Button </Button>
+
+      {/* <CardGroup></CardGroup> */}
     </div>
   );
 }
