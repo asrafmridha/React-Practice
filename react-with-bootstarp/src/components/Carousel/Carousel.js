529cd92d4a52cdb51d35b13591ee5f9a467c9172
asraf-mridha <asrafmridha@gmail.com>
2024-07-05 19:52:46 +0600

    intial commit

--- react-with-bootstarp/src/components/Carousel/Carousel.js
+++ react-with-bootstarp/src/components/Carousel/Carousel.js
@@ -13,36 +13,40 @@ const Carousel = () => {
 
 
     return (
-
         <div id="carouselExampleCaptions" className="carousel slide">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div className="carousel-inner">
-
-                {
-                    banners?.data?.map((banner,index) =>
-                        <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={banner.id}>
-                            <img src={banner.photo} className="d-block w-100" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>{banner.title}</h5>
-                                <p>{banner.slug}</p>
-                            </div>
+            {
+                banners?.data?.map((banner, index) =>
+                    <>
+                        <div className="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                         </div>
-                    )
-                }
+                        <div className="carousel-inner">
 
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+
+                            <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={banner.id}>
+                                <img src={banner.photo} className="d-block w-100" alt="..." />
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>{banner.title}</h5>
+                                    <p>{banner.slug}</p>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </>
+                )
+            }
+
         </div>
 
     );
