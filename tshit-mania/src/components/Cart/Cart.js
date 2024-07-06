34f47e69598f6427863c9e562a3ac8cf52850081
asraf-mridha <asrafmridha@gmail.com>
2024-07-06 21:29:38 +0600

    Remove From Cart And Not Allow Duplicate

--- tshit-mania/src/components/Cart/Cart.js
+++ tshit-mania/src/components/Cart/Cart.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({cart,handleRemoveToCart}) => {
     return (
         <div>
             <h3>Order Summary</h3>
+            <h3>Order Quantity :{cart.length}</h3>
+            {
+                cart.map(cart=><p key={cart._id}>{cart.name}  <button onClick={()=>handleRemoveToCart(cart)}>X</button></p>)
+            }
         </div>
     );
 };
--- tshit-mania/src/components/Home/Home.js
+++ tshit-mania/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../TShirt/Tshirt';
 import Cart from '../Cart/Cart';
@@ -6,15 +6,29 @@ import './Home.css';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+    const handleAddToCart = (tshirt) => {
+        const existingCartItem = cart.find(ts => ts._id === tshirt._id);
+        if (existingCartItem) {
+            alert("Item Already Added In Cart");
+        } else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
+    }
+    const handleRemoveToCart=(tshirt)=>{
+        const remainingItem= cart.filter(ts=>ts._id!==tshirt._id);
+        setCart(remainingItem);
+    }
     return (
         <div className='home-container'>
             <div className="t-shirt-container">
                 {
-                    tshirts.map(tshirt => <Tshirt key={tshirt._id} tshirt={tshirt}></Tshirt>)
+                    tshirts.map(tshirt => <Tshirt key={tshirt._id} tshirt={tshirt} handleAddToCart={handleAddToCart}></Tshirt>)
                 }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart cart={cart} handleRemoveToCart={handleRemoveToCart}></Cart>
             </div>
         </div>
     );
