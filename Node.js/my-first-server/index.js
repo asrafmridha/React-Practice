76fd10821bc55d76922d59305a093d8c46701ca3
asraf-mridha <asrafmridha@gmail.com>
2024-07-12 22:38:15 +0600

    Create Get All Data Api And Single Phone Api

--- Node.js/my-first-server/index.js
+++ Node.js/my-first-server/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 5002;
+
+const users =require('./users.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World From Node!')
+});
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+app.get('/user/:id', (req, res) => {
+    const id=parseInt(req.params.id);
+    const user =users.find(u =>u.id===id) || {};
+    res.send(user);
 });
 
 app.listen(port, () => {
