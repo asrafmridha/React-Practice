3e2529583bae5d0faa72d971727d32b5a1e5ce4d
asraf-mridha <asrafmridha@gmail.com>
2024-07-16 21:01:22 +0600

    Open Google Login Popup And Recap Google Popup Sign In

--- simple-firebase-authentication/src/App.js
+++ simple-firebase-authentication/src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import {getAuth,GoogleAuthProvider} from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import app from './firebase/firebase.init';
 
-const auth =getAuth(app);
+const auth = getAuth(app);
 function App() {
   const provider = new GoogleAuthProvider();
-  const handleSignInWithGoogle=() =>{
-    console.log("Hello Tahmina");
+  const handleSignInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
   return (
     <div className="App">
-        <button onClick={handleSignInWithGoogle}>Sign In With Google</button>
+      <button onClick={handleSignInWithGoogle}>Sign In With Google</button>
     </div>
   );
 }
