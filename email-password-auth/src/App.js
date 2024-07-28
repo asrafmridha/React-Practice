e54f7934a1d575fb5678e50e7d8bff0864bfa3b7
asraf-mridha <asrafmridha@gmail.com>
2024-07-28 22:03:13 +0600

    Simple Form, Input, Change, Blur, Submit, PreventDefault

--- email-password-auth/src/App.js
+++ email-password-auth/src/App.js
@@ -4,12 +4,20 @@ import app from './firebase/firebase.init';
 
 
 const auth =getAuth(app);
+
+const handleRegister=(event)=>{
+  event.preventDefault();
+  const email =event.target.email.value;
+  const password =event.target.email.value;
+  console.log(email,password);
+}
 function App() {
   return (
     <div className="App">
-      <form>
-        <input type="email" name="" id="" placeholder='Your Email' />
-        <input type="password" name="" id="" placeholder='Password' />
+      <form onSubmit={handleRegister}>
+        <input type="email" name="email" id="" placeholder='Your Email' /> <br />
+        <input type="password" name="password" id="" placeholder='Password' /> <br />
+        <button>Register</button>
       </form>
 
     </div>
