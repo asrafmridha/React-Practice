ae60cb9e08297f5fafff996d594278eb58dcb6a0
asraf-mridha <asrafmridha@gmail.com>
2024-07-29 22:42:47 +0600

    Create New User Using Email And Password On Firebase

--- email-password-auth/src/components/RegisterReactBootstarp.js
+++ email-password-auth/src/components/RegisterReactBootstarp.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
 
 const RegisterReactBootstarp = () => {
     const handleRegister=(event)=>{
@@ -9,6 +12,13 @@ const RegisterReactBootstarp = () => {
         const email=target.email.value;
         const password=target.password.value;
         console.log(email,password);
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result=>{
+            const user =result.user;
+            console.log(user)
+        }).catch(error=>{
+            console.error('error', error);
+        })
         
         
 
@@ -19,12 +29,12 @@ const RegisterReactBootstarp = () => {
             <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name='password' placeholder="Password" />
+                    <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
