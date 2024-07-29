c18e6fe38a1dde86436c0ac169f2067cceaea8ff
asraf-mridha <asrafmridha@gmail.com>
2024-07-29 22:22:51 +0600

    Use Bootstrap Form And Get User Email And Password

--- email-password-auth/src/App.js
+++ email-password-auth/src/App.js
@@ -1,6 +1,7 @@
 import {getAuth} from 'firebase/auth';
 import './App.css';
 import app from './firebase/firebase.init';
+import RegisterReactBootstarp from './components/RegisterReactBootstarp';
 
 
 const auth =getAuth(app);
@@ -13,12 +14,8 @@ const handleRegister=(event)=>{
 }
 function App() {
   return (
-    <div className="App">
-      <form onSubmit={handleRegister}>
-        <input type="email" name="email" id="" placeholder='Your Email' /> <br />
-        <input type="password" name="password" id="" placeholder='Password' /> <br />
-        <button>Register</button>
-      </form>
+    <div className="">
+      <RegisterReactBootstarp></RegisterReactBootstarp>
 
     </div>
   );
