449d762523a65e7a68bd4cf8f7dfcebc38ae72bf
asraf-mridha <asrafmridha@gmail.com>
2024-09-06 21:13:11 +0600

    Create client side post and Insert single user data

--- simple-crud-client/src/App.jsx
+++ simple-crud-client/src/App.jsx
@@ -1,11 +1,35 @@
+import { json } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
 
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
   return (
     <>
       <h1>Simple Crud</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="email" name="name" id="" /> <br />
+        <input type="text" name="email" id="" /> <br />
+        <input type="submit" value="ADD USER" />
+      </form>
     </>
   );
 }
--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -25,11 +25,21 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
+
+        const database = client.db("userstDB");
+        const userCollection = database.collection("user");
+
+        app.post('/users', async(req,res)=>{
+            const user=req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+
+        });
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
