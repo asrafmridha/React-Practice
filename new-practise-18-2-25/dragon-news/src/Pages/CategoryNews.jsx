f80af25702a3abddab7bd0b102c155f5d1e20c96
asraf mridha <asrafmridha@gmail.com>
2025-04-29 12:42:33 +0600

    Design The News Category Page with ChatGPT

--- new-practise-18-2-25/dragon-news/src/Pages/CategoryNews.jsx
+++ new-practise-18-2-25/dragon-news/src/Pages/CategoryNews.jsx
@@ -1,12 +1,18 @@
 import React, { use } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsCard from '../components/NewsCard';
 
 const CategoryNews = () => {
-    const {data} = useLoaderData();
+    const {data:news} = useLoaderData();
+    // console.log(news);
 
     return (
         <div>
-            <p>{data.length} Category New Found This Page</p>
+            <h2 className='font-semibold mb-3'>Dragon News Home</h2>
+            <p>{news.length} Category New Found This Page</p>
+            {
+                news.map(singleNews=><NewsCard key={singleNews._id} news={singleNews}></NewsCard>)
+            }
         </div>
     );
 };
