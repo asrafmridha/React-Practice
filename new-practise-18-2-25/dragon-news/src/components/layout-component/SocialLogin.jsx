98b4da3e1dbb5c861415c8697360b97646af0891
asraf mridha <asrafmridha@gmail.com>
2025-05-12 14:39:11 +0600

    Google Login System and env variable setup

--- new-practise-18-2-25/dragon-news/src/components/layout-component/SocialLogin.jsx
+++ new-practise-18-2-25/dragon-news/src/components/layout-component/SocialLogin.jsx
@@ -1,13 +1,25 @@
+import { useContext } from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const SocialLogin = () => {
+  const {signInWithGoogle,setUser} =useContext(AuthContext)
+  const handleGoogleLogin=()=>{
+    signInWithGoogle() 
+    .then((result) => {
+      const user = result.user;
+      setUser(user);
+    }).catch((error) => {
+      console.error("error", error);
+    })
+  }
   return (
     <div className="space-y-3">
       <h2 className="text-xl font-semibold">Login With</h2>
-      <button className="flex items-center w-full gap-2 px-4 py-2 border-2 border-blue-400 rounded-lg hover:bg-blue-50">
+      <button onClick={handleGoogleLogin} className="btn flex items-center w-full gap-2 px-4 py-2 border-2 border-blue-400 rounded-lg hover:bg-blue-50">
         <FaGoogle /> Login with Google
       </button>
-      <button className="flex items-center w-full gap-2 px-4 py-2 border-2 border-gray-400 rounded-lg hover:bg-gray-100">
+      <button className="btn flex items-center w-full gap-2 px-4 py-2 border-2 border-gray-400 rounded-lg hover:bg-gray-100">
         <FaGithub /> Login with Github
       </button>
     </div>
--- new-practise-18-2-25/dragon-news/src/provider/AuthProvider.jsx
+++ new-practise-18-2-25/dragon-news/src/provider/AuthProvider.jsx
@@ -1,59 +1,73 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+  GoogleAuthProvider,
+} from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
 
-export const AuthContext=createContext();
-const AuthProvider = ({children}) => {
+export const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [user,setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signInWithUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+  const updateUserProfile = (updateData) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, updateData);
+  };
+  const signOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-    const createUser = (email, password) => {
-      setLoading(true);
-        return createUserWithEmailAndPassword(auth, email, password);
-      };
-    const signInWithUser = (email, password) => {
-      setLoading(true);
-        return signInWithEmailAndPassword(auth, email, password);
-      };    
-      const updateUserProfile = (updateData) => {
-      setLoading(true);
-        return updateProfile(auth.currentUser,updateData);
-      };
-    const signOutUser = () => {
-      setLoading(true);
-      return signOut(auth);
-      };
-    
-    const authInfo={
-        createUser,
-        user,
-        setUser,
-        signInWithUser,
-        signOutUser,
-        updateUserProfile,
-        loading
+  const authInfo = {
+    createUser,
+    user,
+    setUser,
+    signInWithUser,
+    signOutUser,
+    updateUserProfile,
+    signInWithGoogle,
+    loading,
+  };
 
-    }
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        console.log("Current User Is", currentUser);
+        setUser(currentUser);
+        setLoading(false);
+      } else {
+        console.log("user Is Logged Out");
+        setUser(null);
+      }
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
 
-    useEffect(() => {
-        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-          if (currentUser) {
-            console.log("Current User Is", currentUser);
-            setUser(currentUser);
-            setLoading(false);
-          } else {
-            console.log("user Is Logged Out");
-            setUser(null);
-          }
-          setLoading(false);
-        });
-        return () => {
-          unSubscribe();
-        };
-      }, []);
-    
-    return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
