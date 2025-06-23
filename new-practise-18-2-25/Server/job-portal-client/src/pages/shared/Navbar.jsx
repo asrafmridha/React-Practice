a15567f65349702045b373a9b56152361c358d41
asraf mridha <asrafmridha@gmail.com>
2025-06-23 16:39:06 +0600

    SIgnout footer logout iconset

--- new-practise-18-2-25/Server/job-portal-client/src/pages/shared/Navbar.jsx
+++ new-practise-18-2-25/Server/job-portal-client/src/pages/shared/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router";
 import AuthContext from "../../context/AuthContext";
+import logo from "../../assets/job-favicon.png";
 
 const Navbar = () => {
   const { user, signOutUser, setUser } = useContext(AuthContext);
@@ -41,21 +42,7 @@ const Navbar = () => {
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+            <img src={logo} alt="" />
           </div>
           <ul
             tabIndex={0}
@@ -64,7 +51,11 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">
+          {" "}
+          <img src={logo} alt="" />
+          <h3 className="text-2xl">Job Portal</h3>
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
