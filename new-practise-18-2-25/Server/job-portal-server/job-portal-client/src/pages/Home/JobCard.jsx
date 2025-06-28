189674aea17ab4bf74943a88457b89e4e3c675e2
asraf mridha <asrafmridha@gmail.com>
2025-06-28 16:42:13 +0600

    Display Job Detail Info In The Job Card

--- new-practise-18-2-25/Server/job-portal-server/job-portal-client/src/pages/Home/JobCard.jsx
+++ new-practise-18-2-25/Server/job-portal-server/job-portal-client/src/pages/Home/JobCard.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React from "react";
+import { SiGooglemaps } from "react-icons/si";
 
-const JobCard = ({job}) => {
-    const {_id,applicationDeadline,category,company,company_logo}= job;
-    return (
-        <div>
-          {/* {_id}   */}
+const JobCard = ({ job }) => {
+  console.log(job);
+  const {
+    _id,
+    applicationDeadline,
+    category,
+    company,
+    company_logo,
+    location,
+    title,
+    requirements,
+    description,
+    salaryRange
+  } = job;
+  return (
+    <div className="card bg-base-100 shadow-sm">
+      <div className="flex gap-2 m-3">
+        <figure>
+          <img className="" src={company_logo} alt="Company Logo" />
+        </figure>
+        <div className="flex flex-col justify-center">
+          <h4 className="text-2xl">{company}</h4>
+          <div className="flex items-center gap-1 text-sm text-gray-600">
+            <SiGooglemaps className="text-red-500" />
+            <span>{location}</span>
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="card-body">
+        <h2 className="card-title">
+          {title} <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>{description}</p>
+        <div className="flex flex-wrap gap-4">
+          {requirements.map((requirement, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-1 text-sm text-gray-700"
+            >
+              <span>✅</span>
+              <span>{requirement}</span>
+            </div>
+          ))}
+        </div>
+
+        <div className="card-actions justify-end">
+          <p>$ {salaryRange.max} - {salaryRange.min}</p>
+          <button className="btn btn-primary mt-4">Apply Now</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JobCard;
--- new-practise-18-2-25/Server/job-portal-server/job-portal-client/src/pages/Home/LatestJob.jsx
+++ new-practise-18-2-25/Server/job-portal-server/job-portal-client/src/pages/Home/LatestJob.jsx
@@ -13,7 +13,7 @@ const LatestJob = () => {
         })
     },[])
     return (
-        <div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
             {
                 jobs.map(job=><JobCard key={job._id} job={job}></JobCard>)
             }
