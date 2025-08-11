6fb4f2bbfec43cdf9ce37047be476f84181c2353
asraf mridha <asrafmridha@gmail.com>
2025-08-11 11:27:28 +0600

    Updated

--- new-practise-18-2-25/Server/job-portal-server/index.js
+++ new-practise-18-2-25/Server/job-portal-server/index.js
@@ -30,7 +30,12 @@ async function run() {
         const jobApplicationCollection = client.db("jobPortal").collection('job_application');
         app.get('/jobs', async (req, res) => {
             try {
-                const jobs = await jobCollection.find().toArray();
+                const email=req.query.email;
+                const query={}
+                if(email){
+                    query ={hr_email:email};
+                }
+                const jobs = await jobCollection.find(query).toArray();
                 res.send(jobs);
             } catch (err) {
                 console.error(err);
